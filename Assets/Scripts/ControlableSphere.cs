3168933d3d14cdd7dcfe897085214082db1a3394
Your Name <you@example.com>
2021-02-16 15:25:52 +1000

    Написал управление сферой

--- Assets/Scripts/ControlableSphere.cs
+++ Assets/Scripts/ControlableSphere.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ControlableSphere : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField, Range(0f, 30f)] float maxSpeed = 5f;
+	[SerializeField, Range(0f, 30f)] float maxAcceleration = 10f;
+	Vector3 velocity;
+	Vector3 desiredVelocity;
+	Rigidbody body;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Awake () {
+		body = GetComponent<Rigidbody>();
+	}
+	void Update()
+	{
+		Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+		desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+	}
+
+	void FixedUpdate()
+	{
+		var maxSpeedChange = maxAcceleration * Time.deltaTime;
+		velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+		velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+		body.velocity = velocity;
+	}
+
+	//     void Jump()
+//     {
+//         desiredJump = false;
+//         if (onGround || jumpPhase < maxAirJumps)
+//         {
+//             jumpPhase++;
+//             var jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+//             if (velocity.y > 0f)
+//             {
+//                 jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+//             }
+//             velocity.y += jumpSpeed;
+//         }
+//     }
 }
