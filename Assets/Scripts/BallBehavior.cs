158bfd906313a7adb9711089dfeb55591c9f50da
Lightgazer <lightgazer@tuta.io>
2021-03-04 21:36:12 +1000

    Создал Score Manager

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class BallBehavior : MonoBehaviour
 {
+    Vector3 startPosition;
+    Score manager;
     void Awake()
     {
-        Debug.Log(transform);
+        startPosition = transform.localPosition;
+        var managerObject = GameObject.Find("ScoreManager");
+        manager = managerObject.GetComponent<Score>();
         // body = GetComponent<Rigidbody>();
     }
 
     void OnCollisionEnter(Collision collision)
-	{
-		if(collision.gameObject.name == "Player Wall") {
-			Debug.Log("!!!");
-		}
+    {
+        var res = manager.CheckScore(collision.gameObject);
+
+        if (res == HitStatus.Hit)
+        {
+            transform.localPosition = startPosition;
+        }
     }
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HitStatus {
+    Hit,
+    Other
+}
+
+public class Score : MonoBehaviour
+{
+    [SerializeField, Range(0, 100)] int winScore;
+
+    public HitStatus CheckScore(GameObject hit) {
+        var name = hit.name;
+        if (name == "Player Wall" || name == "Enemy Wall") {
+            return HitStatus.Hit;
+        }
+        return HitStatus.Other;
+    }
+}
