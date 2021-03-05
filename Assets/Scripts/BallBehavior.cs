04364f2af132fdacd5422df9692b5c0bf136232d
Your Name <you@example.com>
2021-03-05 11:42:25 +1000

    Ведение счёта и конец игры

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class BallBehavior : MonoBehaviour
 {
     Vector3 startPosition;
+    Vector3 disabledPostion = new Vector3(0f, 3f, 0f);
     Score manager;
+    Rigidbody body; 
     void Awake()
     {
         startPosition = transform.localPosition;
         var managerObject = GameObject.Find("ScoreManager");
         manager = managerObject.GetComponent<Score>();
-        // body = GetComponent<Rigidbody>();
+        body = GetComponent<Rigidbody>();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -21,6 +23,10 @@ public class BallBehavior : MonoBehaviour
         if (res == HitStatus.Hit)
         {
             transform.localPosition = startPosition;
+            body.velocity = new Vector3(0f, 0f, 0f);
+        } else if (res == HitStatus.EndGame) {
+            transform.localPosition = disabledPostion;
+            body.velocity = new Vector3(0f, 0f, 0f);
         }
     }
 }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,21 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public enum HitStatus {
+public enum HitStatus
+{
     Hit,
+    EndGame,
     Other
 }
 
 public class Score : MonoBehaviour
 {
     [SerializeField, Range(0, 100)] int winScore;
+    int playerScore = 0;
+    TextMeshProUGUI playerText;
+    int enemyScore = 0;
+    TextMeshProUGUI enemyText;
 
-    public HitStatus CheckScore(GameObject hit) {
+    private void Awake()
+    {
+        playerText = GameObject.Find("Player Score").GetComponent<TextMeshProUGUI>();
+        enemyText = GameObject.Find("Enemy Score").GetComponent<TextMeshProUGUI>();
+    }
+
+    public HitStatus CheckScore(GameObject hit)
+    {
+        var status = HitStatus.Other;
         var name = hit.name;
-        if (name == "Player Wall" || name == "Enemy Wall") {
-            return HitStatus.Hit;
+        if (name == "Player Wall")
+        {
+            enemyScore++;
+            status = HitStatus.Hit;
         }
-        return HitStatus.Other;
+        else if (name == "Enemy Wall")
+        {
+            playerScore++;
+            status = HitStatus.Hit;
+        }
+
+        ShowScore();
+        var endGame = CheckWin();
+        if(endGame) status = HitStatus.EndGame;
+
+        return status;
+    }
+
+    private void ShowScore()
+    {
+        playerText.text = playerScore.ToString();
+        enemyText.text = enemyScore.ToString();
+    }
+
+    private bool CheckWin()
+    {
+        if (playerScore >= winScore)
+        {
+            ShowMessage("Win Message");
+            return true;
+        }
+        else if (enemyScore >= winScore)
+        {
+            ShowMessage("Lose Message");
+            return true;
+        }
+        return false;
+    }
+
+    // где-то здесь нужно начать выделать отдельный класс
+    private void ShowMessage(string name)
+    {
+        var transform = GameObject.Find(name).GetComponent<RectTransform>();
+        StartCoroutine(AnimationCoroutine(transform));
+    }
+
+
+    IEnumerator AnimationCoroutine(RectTransform transform)
+    {
+        float speed = 4;
+        while (transform.anchoredPosition.x != 0)
+        {
+            if (transform.anchoredPosition.x > 0) transform.anchoredPosition = new Vector2(transform.anchoredPosition.x - speed, 0);
+            else transform.anchoredPosition = new Vector2(transform.anchoredPosition.x + speed, 0);
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
