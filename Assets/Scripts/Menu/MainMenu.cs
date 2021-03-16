9a25f2e390dbfda0de270bc59bb3a976c791adea
Your Name <you@example.com>
2021-03-16 17:43:05 +1000

    basic menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        var index = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit();
+    }
+}
